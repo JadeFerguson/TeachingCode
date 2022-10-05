24aeda239860d4ec38a4665afba516224b2ad870
Jade Ferguson <jadesnider2440@gmail.com>
2022-10-05 15:05:51 -0700

    Scaffolded ASP.NET identity into project

--- TeachingCode/Program.cs
+++ TeachingCode/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+                                                                                            // Change this to false to make it so there is no email confimation
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
